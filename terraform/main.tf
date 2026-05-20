f66be46a8aab3a0c7293ced2bfc28b7e3b14dd61
raj31rohit <raj31.rohit.com>
2026-05-20 12:32:27 +0530

    updating TF code versions

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,11 +24,14 @@ resource "aws_s3_bucket" "deployment_bucket" {
       }
     }
   }
-
-  public_access_block {
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
-  }
 }
+
+resource "aws_s3_bucket_public_access_block" "deployment_bucket_block" {
+  bucket = aws_s3_bucket.deployment_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
